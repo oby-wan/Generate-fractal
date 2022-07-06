8ed0927b1c2e7a21ce153fc232b5a672b2703ba0
Parivash Gholipour <108769801+ParivashG@users.noreply.github.com>
2022-07-05 22:50:38 -0700

    Parivash commit 1

--- Complex.hpp
+++ Complex.hpp
@@ -1,18 +1,19 @@
 #pragma once
 #include <iostream>
+using namespace std;
 
-
-class Complex{
-
-protected:
+class Complex
+{
+private:
 	double imag;
 	double real;
-
 public:
-	~Complex();
 	Complex();
+	~Complex();
 	Complex(const Complex&);
 	Complex(double, double);
 	double& operator[](const char*);
-
-}
+	friend const Complex operator*(const Complex&, const Complex&);
+	friend const Complex operator+(const Complex&, const Complex&);
+	friend double getMagnitudeSquared(const Complex&);
+};
