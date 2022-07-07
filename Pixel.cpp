46ab7b94830de8bcdb0c26a8555040e8c7813151
Parivash Gholipour <108769801+ParivashG@users.noreply.github.com>
2022-07-07 16:01:00 -0700

    Update Pixel.cpp

--- Pixel.cpp
+++ Pixel.cpp
@@ -1,32 +1,40 @@
-#include <iostream>
 #include "Pixel.hpp"
 #include "Fractal.hpp"
 
-using namespace std;
-
-Pixel::~Pixel() {
-	cout << "pixel destructor called" << endl;
-	delete[] p;
-	p = nullptr;
-
-}
-const unsigned int& Pixel::operator[](const char* c){
-	
-}
-
-Pixel::Pixel() : blue(0), green(0), red(0) {
+Pixel::Pixel():red(0),green(0),blue(0)
+{
 	cout << "default constructor called" << endl;
 }
 
-Pixel::Pixel(const Pixel& p) : blue(p.blue), green(p.green), red(p.red) {
+Pixel::~Pixel() 
+{
+	cout << "pixel destructor called" << endl;
+}
+
+Pixel::Pixel(const Pixel& a) : red(a.red) , blue(a.blue), green(a.green)
+{
 	cout << "Copy constructor called" << endl; 
 }
 
-Pixel::Pixel(unsigned int blue, unsigned int green, unsigned int red) : blue(0), green(0), red(0) {
+Pixel::Pixel(unsigned int r, unsigned int g, unsigned int b) : red(r), blue(b), green(g)
+{
 	cout << "3-arg constructor called" << endl;
-
-
 }
-unsigned int Pixel::converToPixel(unsigned int color) {
 
+const unsigned int& Pixel::operator[](const char* a)
+{
+	if (strcmp(a, "red") == 0)
+		return red;
+	else if (strcmp(a, "green") == 0)
+		return green;
+	else if (strcmp(a, "blue") == 0)
+		return blue;
+	else
+		exit(1);
 }
+
+ofstream& operator << (ofstream& os, const Pixel& p)
+{
+	os << p["red"] << " " <<  p["green"] << " " << p["blue"] << " ";
+	return os;	
+} 
