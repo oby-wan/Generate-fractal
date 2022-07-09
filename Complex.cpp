914d3d23e82ceaecceebdb0965c0a37f00dc58aa
Raffi Macaraig <108770810+DaRafster@users.noreply.github.com>
2022-07-08 22:14:13 -0700

    commit 10 raffi

--- Complex.cpp
+++ Complex.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 #include "Complex.hpp"
-using namespace std;
 
-Complex::~Complex() 
-{
-	cout << "complex destructor called" << endl;
-}
+using namespace std;
 
 Complex::Complex() : imag(0.0), real(0.0)
 {
-	cout << "default constructor called" << endl;
+	// doesn't need cout statement
 }
 
 Complex::Complex(const Complex& a) : imag(a.imag), real(a.real)
 {
-	cout << "Copy constructor called" << endl;
-
+	// doesn't need cout statement
 }
 
 Complex::Complex(double a, double b) : imag(a), real(b)
 {
-	cout << "Parameterized constructor called" << endl;
+	// doesn't need cout statement
 }
 
 double& Complex::operator[](const char* a)
@@ -36,21 +31,31 @@ double& Complex::operator[](const char* a)
 const Complex operator*(const Complex& a, const Complex& b)
 {
 	Complex x;
+
 	x.real = a.real * b.real - a.imag * b.imag;
 	x.imag = a.real * b.imag + a.imag * b.real;
+
 	return x;
 }
 
 const Complex operator+(const Complex& a, const Complex& b)
 {
 	Complex x;
+
 	x.imag = (a.imag) + (b.imag);
 	x.real = (a.real) + (b.real);
+
 	return x;
 }
 
 double getMagnitudeSquared(const Complex& a)
 {
-	double c = (a.imag)*(a.imag) + (a.real)*(a.real);
-	return c;
+	double squaredMag = (a.imag * a.imag) + (a.real * a.real);
+
+	return squaredMag;
+}
+
+Complex::~Complex()
+{
+	// doesn't need cout statement
 }
