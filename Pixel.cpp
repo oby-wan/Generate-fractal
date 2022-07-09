f8001e62732febe247b8ddf0e217fb6143fe8e65
Raffi Macaraig <108770810+DaRafster@users.noreply.github.com>
2022-07-08 22:15:24 -0700

    Update Pixel.cpp

--- Pixel.cpp
+++ Pixel.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 #include "Pixel.hpp"
 #include "Fractal.hpp"
+#include <fstream>
 
 using namespace std;
 
-Pixel::~Pixel() {
-	cout << "pixel destructor called" << endl;
-	delete[] p;
-	p = nullptr;
-
+Pixel::~Pixel() 
+{
+	// don't need cout statement
 }
-const unsigned int& Pixel::operator[](const char* c){
-	
+const unsigned int& Pixel::operator[](const char* c) 
+{
+	if (strcmp(c, "red") == 0)
+		return red;
+	else if (strcmp(c, "blue") == 0)
+		return blue;
+	else if (strcmp(c, "green") == 0)
+		return green;
+	else 
+	{
+		cout << "> ERROR: invalid index. Exiting..." << endl;
+		exit(1);
+	}
 }
 
-Pixel::Pixel() : blue(0), green(0), red(0) {
-	cout << "default constructor called" << endl;
+Pixel::Pixel() : blue(0), green(0), red(0) 
+{
+	// don't need cout statement
 }
 
-Pixel::Pixel(const Pixel& p) : blue(p.blue), green(p.green), red(p.red) {
-	cout << "Copy constructor called" << endl; 
+Pixel::Pixel(const Pixel& p) : blue(p.blue), green(p.green), red(p.red) 
+{
+	// don't need cout statement
 }
 
-Pixel::Pixel(unsigned int blue, unsigned int green, unsigned int red) : blue(0), green(0), red(0) {
-	cout << "3-arg constructor called" << endl;
-
-
+Pixel::Pixel(unsigned int b, unsigned int g, unsigned int r) : blue(b), green(g), red(r) 
+{
+	// don't need cout statement
 }
-unsigned int Pixel::converToPixel(unsigned int color) {
 
+ofstream& operator<<(ofstream& os, Pixel& p) 
+{
+	os << p["red"] << " " << p["green"] << " " << p["blue"] << " ";
+
+	return os;
 }
